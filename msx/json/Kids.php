83620660cc6537459b1b450051907f62e1cf513b
AmmuBhai22 <109035027+AmmuBhai22@users.noreply.github.com>
2023-12-20 21:44:55 +0530

    update msx/json/Kids.php

--- msx/json/Kids.php
+++ msx/json/Kids.php
@@ -9,12 +9,12 @@
           "type": "seperate",
           "layout": "2,2,3,4",
           "title": "AnimeWorld{br}Server 1",
-          "image": "http://phpkrotest.000webhostapp.com/msx/Backgrounds/S1.jpg",
+          "image": "https://ammubhai22-github-io.pages.dev/msx/images/S1.jpg",
           "color": "msx-black",
           "imageFiller": "cover",
           "imageOverlay": 0,
           "alignment": "title-center",
-          "action": "content:http://phpkrotest.000webhostapp.com/msx/Json/NewLayout/Anime.json"
+          "action": "content:https://ammubhai22-github-io.pages.dev/msx/json/Anime.json"
         }
       ]
     }
